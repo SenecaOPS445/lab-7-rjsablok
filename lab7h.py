c90438def397ccac33c846ae8b350ae1d42dd709
ram sablok <rjsablok@myseneca.ca>
2025-03-21 15:37:14 -0400

    Added

---
+++ lab7h.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Student ID: rjsablok
+
+
+def function1():
+    print('print() in function1 on schoolName:',schoolName)
+
+def function2():
+    print('print() in function2 on schoolName:',schoolName)
+
+schoolName = 'Seneca Polytechnic'
+print('print() in main on schoolName:',schoolName)
+function1()
+print('print() in main on schoolName:',schoolName)
+function2()
+print('print() in main on schoolName:',schoolName)
